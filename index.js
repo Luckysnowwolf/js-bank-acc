0fc04cef7b0632623b8791b87e88023d3210d23a
Luckysnowwolf <179121658+Luckysnowwolf@users.noreply.github.com>
2024-11-06 12:33:38 +0100

    added functions and const

---
+++ index.js
@@ -0,0 +1,69 @@
+const bankacc = {
+    Name: "star",
+    country: "Sweden",
+    language: "swedish",
+    balance: 50000,
+    curency: "kronor",
+    active: true,
+}
+const pay = {
+    montly: 45000,
+    yearly: 540000,
+}
+const tax = {
+    tax: 20%
+}
+const card = {
+    cardbalance: 2000,
+    curency: "kronor",
+}
+
+const otherBankAcc = {
+    Name: "dark",
+    country: "amaerica",
+    language: "english",
+    balance:3000,
+    curency: "dollar",
+    active: true,
+}
+let withdraw {
+    
+}
+
+function getBalance1{
+console.log(bankacc(balance))
+}
+function getBalance2{
+console.log(otherBankAcc(balance))
+}
+
+function withdrawal{
+if (withdraw >= (bankacc(balance))){ true 
+
+} else(withdraw <  (bankacc(blance)))
+{
+    false(bankacc(balance))
+}
+console.log()
+
+}
+function deposit{
+    if (card(cardbalance > 0 )){ true
+
+    } else(card(cardbalance < 0 )){
+        false
+    }
+    console.log()
+}
+function getAccName1{
+    console.log(bankacc(Name))
+
+}
+function getAccName2{
+    console.log(otherBankAcc(Name))
+
+}
+
+function accError{
+
+}
